0f9ef532cb4d73c24947b047f0f0958fb3c3bee0
Dmitry Kolbenev <149693292+kolbenev@users.noreply.github.com>
2026-01-26 17:08:08 +0300

    Remove top gradient div from Hero component
    Removed an unnecessary top gradient div in the Hero component.

--- src/myPortfolio/components/Hero.jsx
+++ src/myPortfolio/components/Hero.jsx
@@ -151,12 +151,6 @@ export default function Hero() {
           <br />и я программист.
         </motion.h1>
       </motion.div>
-
-      <div
-        className="absolute top-0 left-0 w-full h-40
-        bg-gradient-to-b from-[#0A0A0F] via-[#0A0A0F]/80 to-transparent pointer-events-none"
-      />
-
       <div
         className="absolute bottom-0 left-0 w-full h-40
         bg-gradient-to-t from-[#0A0A0F] via-[#0A0A0F]/80 to-transparent pointer-events-none"
